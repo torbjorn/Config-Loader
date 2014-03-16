894028408543b6b3dc9274d76b0a967f876e7265
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-03-16 04:45:23 +0100

    good progress

--- lib/Config/Loader.pm
+++ lib/Config/Loader.pm
@@ -48,6 +48,15 @@ sub new_source {
 
 use File::Spec::Functions;
 use File::Basename qw(fileparse);
+use Config::Any;
+
+## the files we know
+sub _get_extensions { @{ Config::Any->extensions } }
+
+sub _has_extension {
+    my ($f,$d,$ext) = fileparse shift, qr/\.[^.]*/;
+    return $ext && 1;
+}
 
 ## Create input that Profile::Default can work with
 sub _process_args {
@@ -58,65 +67,109 @@ sub _process_args {
     ## 3) Can the P::Default be shoehorned to do this?
     my %args = @{$_[0]};
 
-    $args{sources} = [];
+    my %args_for_p_default = ( sources => [] );
 
-    my $zomg_name = delete $args{name};
+    my @files;
+
+    use Data::Dumper;
+    # print Dumper \%args;
+
+    my $zomg_name = $args{name};
+    my $zomg_path = $args{path};
+    my $zomg_file = $args{file};
+    my $add_local = not $args{no_local};
+    my $add_env   = not $args{no_env};
+
+    my $zomg_env  = defined($zomg_name) ?
+        $ENV{ uc $zomg_name . "_CONFIG" } : undef;
 
     ## Get to work...
-    if ( defined $zomg_name ) {
 
-        ## One or more config files. Delete the path key in the
-        ## process as ::Default don't use them
+    ## Filenames come in env variables
+    if ( $add_env ) {
 
-        my @files;
-
-        ## Filenames come in env variables
-        if ( defined (my $env_filepath =
-                          $ENV{ uc $zomg_name . "_CONFIG" } )) {
-
-            push @files, $env_filepath;
+        ## Either one var configured
+        if (defined $zomg_env) {
+            push @files, $zomg_env, _create_local_siblings($zomg_env)
+                if defined $zomg_env;
 
         }
-        ##.. or are found locally
-        elsif ( defined ( my $dir = delete $args{path} ) ) {
+        ## .. or optional candidates
+        elsif ( defined $args{env_lookup} ) {
 
-            ## Extra verbose variable names at this point
-            my $fname = $zomg_name;
+            $args{env_lookup} = [$args{env_lookup}] unless
+                ref $args{env_lookup} eq "ARRAY";
 
-            my $filepath = catfile( $dir, $fname );
+            my @env_names = map { uc $_ . "_CONFIG" } @{$args{env_lookup}};
 
-            push @files, $filepath;
-
-        }
-
-        ## Add them as sources
-        for my $file (@files) {
-
-            push @{$args{sources}},
-                [ 'File', { file => $file } ];
-
-            ## also push a _local sibling, ::File does the right thing if
-            ## it doesn't exist.
-            if ( not $file =~ /_local/ ) {
-
-                my($new_fname,$dirs,$ext) = fileparse $file, qr/\.[^.]*/;
-
-                ## in time the "local" part will be dynamic
-                $new_fname .= "_" . "local" . $ext;;
-
-                push @{$args{sources}},
-                    [ 'File', { file => catfile($dirs,$new_fname) } ];
-
-            }
-
+            my @files_found = grep { defined $_ } @ENV{@env_names};
+            push @files, @files_found, _create_local_siblings(@files_found);
 
         }
 
     }
+    if ( not @files ) {
+        ##.. or are found locally
+        if ( defined $zomg_file ) {
+            push @files, $zomg_file
+        }
+        ##.. one way or another
+        elsif ( defined $zomg_path and defined $zomg_name ) {
+            my $fp = catfile( $zomg_path, $zomg_name );
 
-    return \%args;
+            push @files, $fp, $add_local ?
+                _create_local_siblings($fp) : ();
+
+        } elsif ( defined $zomg_path ) {
+
+            push @files, $zomg_path, $add_local ?
+                _create_local_siblings($zomg_path) : ();
+
+        }
+    }
+
+    ## files without extension gets all
+    my @files_without_extension = grep { not _has_extension $_ } @files;
+    my @files_with_extension = grep { _has_extension $_ } @files;
+
+    for my $file_wo (@files_without_extension) {
+        push @files_with_extension, map {
+            $file_wo . "." . $_
+        } _get_extensions;
+    }
+
+    ## sort them to match ZOMG::Loader line 91
+    ## should have seen this coming sooner
+    @files_with_extension = sort {
+
+        my $a3 = (my $a2 = $a) =~ s/_local//;
+        my $b3 = (my $b2 = $b) =~ s/_local//;
+
+            return
+                $a3 cmp $b3
+                    ||
+                 $a2 cmp $b2
+
+    } @files_with_extension;
+
+    ## Add them as sources
+    for my $file (grep {-f} @files_with_extension) {
+
+        push @{$args_for_p_default{sources}},
+            [ 'File', { file => $file } ];
+
+    }
+
+    # print Dumper \@files_with_extension;
+
+    return \%args_for_p_default;
 
 };
+
+## return files that have been found and processed
+sub found {
+    ## TODO - THIS IS WHERE I AM NOW
+}
 
 sub _make_object {
     my $self = shift;
@@ -145,6 +198,32 @@ sub reload {
     my $self = shift;
     $self->_make_object;
     return 1;
+}
+
+sub _create_local_siblings {
+
+    my @files = @_;
+    my @local_siblings;
+
+    for my $file (@files) {
+
+        die "blew up" unless defined $file;
+
+        if ( not $file =~ /_local/ ) {
+
+            my($new_fname,$dirs,$ext) = fileparse $file, qr/\.[^.]*/;
+
+            ## in time, the "local" part will be dynamic
+            $new_fname .= "_" . "local" . $ext;
+
+            push @local_siblings, catfile($dirs,$new_fname);
+
+        }
+
+    }
+
+    return @local_siblings;
+
 }
 
 1;
