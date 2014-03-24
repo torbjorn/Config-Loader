da49ce3857ca1dd24b97752faa82370d7913612c
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-03-25 00:48:25 +0100

    decided not to support a list of files as input

--- lib/Config/Loader/Source/Files.pm
+++ lib/Config/Loader/Source/Files.pm
@@ -3,21 +3,43 @@ package Config::Loader::Source::Files;
 use Moo;
 extends 'Config::Loader::Source::Merged';
 
-around BUILDARGS => sub {
-    my ($orig, $class) = (shift, shift);
+## create a 'sources' from supplied 'files' that ::Merged would accept
+##
+## Technically this doesn't prevent other sources, and technically
+## there is no need to, so its available if someone wants to take the
+## trouble, but we help loading files
+sub BUILDARGS {
+    my ($class, @args) = @_;
 
-    if (
-        (ref($_[0])||'') ne 'HASH' and
-        not ( ($_[0]||'') eq 'files' and defined $_[1] ) ) {
+    my $build_sources_in_hash = sub {
 
-        my @sources = map {
-            [ File => { file => $_ } ],
-        } @_;
+        my $args = shift;
 
-        return { sources => \@sources };
+        $args->{sources} //= [];
+
+        ## add real sources from 'files' input
+        push @{ $args->{sources} },
+            map { [ File => { file => $_ } ] }
+            @{delete $args->{files} or []}
+
+    };
+
+    if (  (ref($args[0])||'') eq 'HASH'  ) {
+
+        $build_sources_in_hash->($args[0]);
+        return $args[0];
+
+    }
+    ## pass it as is and let any errors bouble
+    else {
+
+        my $args = {@args};
+        $build_sources_in_hash->($args);
+
+        return $args;
+
     }
 
-    return $class->$orig(@_);
 };
 
 1;
