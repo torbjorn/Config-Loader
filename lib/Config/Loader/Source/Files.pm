032cd6a603ec8e8a71dfba2d91021f4f442bc677
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-03-25 00:56:46 +0100

    comments

--- lib/Config/Loader/Source/Files.pm
+++ lib/Config/Loader/Source/Files.pm
@@ -3,11 +3,13 @@ package Config::Loader::Source::Files;
 use Moo;
 extends 'Config::Loader::Source::Merged';
 
-## create a 'sources' from supplied 'files' that ::Merged would accept
+## Transform the supplied files => [..] to a sources => [...] that is
+## something that ::Merged would accept as sources
 ##
 ## Technically this doesn't prevent other sources, and technically
-## there is no need to, so its available if someone wants to take the
-## trouble, but we help loading files
+## there is no need to, so this module could load other sources if
+## someone wants to take the trouble, but we help loading files only,
+## for other sources, the user is on his own
 sub BUILDARGS {
     my ($class, @args) = @_;
 
