32173dffa01c63e707ce34a2c05046880954abd8
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-04-08 02:55:53 +0200

    changed local_suffix to a builder that looks in %ENV

--- lib/Config/Loader/SourceRole/FileLocalSuffix.pm
+++ lib/Config/Loader/SourceRole/FileLocalSuffix.pm
@@ -14,11 +14,16 @@ has no_local => (
 );
 
 has local_suffix => (
-   is => 'ro',
-   default => quote_sub q{ 'local' },
+   is => 'lazy',
+   default => 'local',
+   builder => sub {
+       my $self = shift;
+       return unless $self->name;
+       return $ENV{ uc $self->name . '_CONFIG_LOCAL_SUFFIX' };
+   }
 );
 
-requires qw/sources/;
+requires qw/name sources/;
 
 ## Simply add an additional File source for every file, that has a
 ## _local suffix in it.
@@ -34,20 +39,26 @@ before _build_loader => sub {
 
         if ( $source->[0] eq "File" ) {
 
-            my $file = $source->[1]{file};
+            if ( exists $source->[1]{file} ) {
 
-            ## This assumes $file is a file or a stem. Cases where it
-            ## is a directory needs to be explored later
-            my( $name, $dirs, $suffix ) = fileparse( $file, qr/\.[^.]*/ );
+                my %source_args = %{$source->[1]};
+                my $file = delete $source_args{file};
 
-            my $new_with_local = $name . "_" . $self->local_suffix;
+                ## This assumes $file is a file or a stem. Cases where it
+                ## is a directory needs to be explored later
+                my( $name, $dirs, $suffix ) = fileparse( $file, qr/\.[^.]*/ );
 
-            my $new_local_file = catfile( $dirs, $new_with_local );
+                my $new_with_local = $name . "_" . $self->local_suffix;
 
-            $new_local_file .= $suffix ? $suffix : "";
+                my $new_local_file = catfile( $dirs, $new_with_local );
 
-            push @local_sources,
-                [ File => { file => $new_local_file } ];
+                $new_local_file .= $suffix ? $suffix : "";
+                $source_args{file} = $new_local_file;
+
+                push @local_sources,
+                    [ File => { %source_args } ];
+
+            }
 
         }
 
